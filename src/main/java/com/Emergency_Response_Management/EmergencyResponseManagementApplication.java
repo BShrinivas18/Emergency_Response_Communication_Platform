4dd51f02ffa7e53437987b5fb5dffd890a060f25
BShrinivas18 <shrinivasbhosale2000@gmail.com>
2024-10-30 11:20:56 +0530

    Initial commit

---
+++ src/main/java/com/Emergency_Response_Management/EmergencyResponseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.Emergency_Response_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyResponseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyResponseManagementApplication.class, args);
+	}
+
+}
